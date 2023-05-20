89d3770f1b952960ebbeb33e16986f9f947cc788
mshreeharsha <shreeharsha.mbhat@gmail.com>
2023-05-20 22:57:40 +0530

    Process Controller Buttons and Form Added

--- MFT/mftFirstFit.js
+++ MFT/mftFirstFit.js
@@ -1,37 +1,58 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("partitionsNoBtn").addEventListener("click", function () {
-        displayPartitionSize();
-    });
+var noOfPartitions = 0;
+
+$(document).ready(function () {
+  $("#noOfPartitionsBtn").click(function () {
+    displayPartSize();
+  });
 });
 
-
-  function displayPartitionSize() {
-    var noOfPartitions = Number(document.getElementById("partitionsNo").value);
-    var htmlText = "";
-    
-    for (var i = 1; i <= noOfPartitions; i++) {
-      htmlText += `
-        <div class="form-group">
-          <label>Size of partition ${i}: </label>
-          <input type="text" class="form-control" id="part-size-${i}" placeholder="Enter size of partition ${i}">
-          <br>
-        </div>
+function displayPartSize() {
+  noOfPartitions = Number($("#noOfPartitions").val());
+  var htmlText = '';
+  var i;
+  for (i = 1; i <= noOfPartitions; i++) {
+    htmlText +=
+      `
+      <div class="form-group">
+          <label>Size of partition ` + String(i) + ` : </label>
+          <input type="text" class="form-control" id="part-size-` + String(i) + `" placeholder="Enter size of partitition ` + String(i) + `">
+      </div>
       `;
-    }
-    
-    htmlText += `
-      <button type="submit" class="btn btn-primary" id="parts-size-btn">Submit</button>
-    `;
-    
-    document.getElementById("PartitionSizeForm").innerHTML = htmlText;
-    
-    document.addEventListener("DOMContentLoaded", function () {
-      document.getElementById("parts-size-btn").addEventListener("click", function () {
-        startColumn2();
-      });
-    });
   }
-  
+  htmlText +=
+    `
+  <button type="submit" class="btn btn-primary" id="parts-size-btn">Submit</button>
+  `;
+  $("#partitionSizeForm").html(htmlText);
+  $(document).ready(function () {
+    $("#parts-size-btn").click(function () {
+      processController();
+    });
+  });
+}
 
-  
+function processController() {
+  var htmlText =
+    `
+  <h2 class = "mt-5"> Process Controller </h2>
+  <button type="submit" class="btn btn-primary" id="addProcessButton">Add process</button>
+  <button type="submit" class="btn btn-primary" id="endProcessButton">Remove process</button>
+  `;
+  $("#processControllerBtns").html(htmlText);
+  var htmlText =
+    `<h3>Physical(User) Memory</h3>
+  <canvas id="myCanvas" width="170" height="520" style="border:2px solid red;"></canvas>
+  `;
+  $("#canvas").html(htmlText);
+  drawPartMemory();
+  $(document).ready(function () {
+    $("#addProcessButton").click(function () {
+      addProcessSize();
+    });
+    $("#endProcessButton").click(function () {
+      remProcessId();
+    });
+  });
+
+}
