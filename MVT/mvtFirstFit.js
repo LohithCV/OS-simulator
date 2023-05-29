5c1529936af98a6cd209e591b236103c86be9ed7
mshreeharsha <shreeharsha.mbhat@gmail.com>
2023-05-29 10:15:13 +0530

    MVT First Fit Added

--- MVT/mvtFirstFit.js
+++ MVT/mvtFirstFit.js
@@ -7,23 +7,20 @@ var total_mem_size = 0;
   var xstart = 10;
   var ystart = 10;
 
-  var part_myCanvas_start = [];
-  var part_myCanvas_end = [];
-
   //Partition Details
   var numberOfPartition = 0;
   var partition_size = [];
   var partition_start = [];
-  var partation_end = [];
+  var partition_end = [];
   var partition_process_id = [];
 
   //Process Details
   var current_process_id = 0;
 
-  var part_occupied = [];
-  var input_q_pro_id = [];
-  var input_q_pro_size = [];
-  var input_q_size = 0;
+  //Queue details
+  var input_queue_process_id = [];
+  var input_queue_process_size = [];
+  var input_queue_size = 0;
 
   $(document).ready(function () {
     $("#memorySizeBtn").click(function () {
@@ -70,6 +67,7 @@ var total_mem_size = 0;
       $("#add-btn").click(function () {
         var process_size = Number($("#addProcessSize").val());
         current_process_id += 1;
+        $("#addProcessSize").val("");
         addProcess(process_size, current_process_id, 0);
       });
     });
@@ -160,8 +158,8 @@ var total_mem_size = 0;
   function sortPart() {
     var i;
     var j;
-    for (i = 0; i < noOfPartitions; i++) {
-      for (j = 0; j < (noOfPartitions - i - 1); j++) {
+    for (i = 0; i < numberOfPartition; i++) {
+      for (j = 0; j < (numberOfPartition - i - 1); j++) {
         if (partition_start[j] > partition_start[j + 1]) {
           var temp = partition_start[j];
           partition_start[j] = partition_start[j + 1];
@@ -183,6 +181,146 @@ var total_mem_size = 0;
     }
   }
 
+  //Ending an Process
+  function endProcessId() {
+    var htmlText =
+      `
+    <div class="form-group">
+        <label>Id of process to be removed: </label>
+        <input type="text" class="form-control" id="rem-pro-id" placeholder="Enter id of process to be removed">      
+    </div>
+    <button type="submit" class="btn btn-primary" id="rem-btn">Remove</button>
+    `;
+    $("#addProcess").html(htmlText);
+    $(document).ready(function () {
+      $("#rem-btn").click(function () {
+        var id_pro = Number($("#rem-pro-id").val());
+        $("#rem-pro-id").val("");
+        endProcess(id_pro);
+      });
+    });
+  }
+
+  function endProcess(process_id) {
+    var i;
+    var found = 0;
+    for (i = 0; i < numberOfPartition; i++) {
+      if (partition_process_id[i] == process_id && found == 0) {
+
+        var j;
+        for (j = i + 1; j < numberOfPartition; j++) {
+            //Shifting for deleting process related Details
+          partition_process_id[j - 1] = partition_process_id[j];
+
+          partition_start[j - 1] = partition_start[j];
+
+          partition_end[j - 1] = partition_end[j];
+
+          partition_size[j - 1] = partition_size[j];
+        }
+        found = 1;
+        numberOfPartition -= 1;
+        break;
+      }
+    }
+    if (found == 1) {
+      drawPart();
+      var i;
+      for (i = 0; i < input_queue_size; i++) {
+        addProcess(input_queue_process_size[i], input_queue_process_id[i], 1);
+      }
+    }
+    else {
+      alert("Process-" + String(process_id) + " not found in memory");
+    }
+    drawInputQTable();
+  }
+
+  //Queue Operations
+
+  function addToQ(process_size, process_id) {
+    input_queue_size += 1;
+    input_queue_process_id[input_queue_size - 1] = process_id;
+    input_queue_process_size[input_queue_size - 1] = process_size;
+  }
+
+  function removeFromQ(process_id) {
+    var i;
+    for (i = 0; i < input_queue_size; i++) {
+      if (input_queue_process_id[i] == process_id) {
+        for (j = i + 1; j < input_queue_size; j++) {
+
+            //Shifting the Contents of the Array
+          input_queue_process_id[j - 1] = input_queue_process_id[j];
+
+          input_queue_process_size[j - 1] = input_queue_process_size[j];
+        }
+      }
+    }
+    input_queue_size -= 1;
+  }
+
+
+  function drawInputQTable() {
+    var htmlText =
+      `
+    <button type="submit" class="btn btn-primary md-3 mt-3" id="compact-btn">Compact</button> 
+    <table class = 'mt-3 table table-bordered border-primary
+    '>
+    <h3>Input Queue</h3>
+    <tr>
+        <th>Process Id</th>
+    `;
+    for (var i = 0; i < input_queue_size; i++) {
+      htmlText +=
+        `
+        <td>` + input_queue_process_id[i] + `</td>
+        `;
+    }
+
+    htmlText +=
+      `
+    <tr>
+        <th>Process Size</th>
+    `;
+    for (var i = 0; i < input_queue_size; i++) {
+      htmlText +=
+        `
+        <td>` + input_queue_process_size[i] + `</td>
+        `;
+    }
+
+    htmlText +=
+      `
+    </tr>
+    </table>
+    `;
+    $("#input-q-table").html(htmlText);
+    $(document).ready(function () {
+      $("#compact-btn").click(function () {
+        Compact();
+      });
+    });
+  }
+
+  //For Compaction
+  function Compact() {
+    var i;
+    for (i = 0; i < numberOfPartition; i++) {
+      if (i == 0) {
+        partition_start[i] = 0;
+        partition_end[i] = partition_start[i] + partition_size[i];
+      }
+      else {
+        partition_start[i] = partition_end[i - 1];
+        partition_end[i] = partition_start[i] + partition_size[i];
+      }
+    }
+    drawPart();
+    for (i = 0; i < input_q_size; i++) {
+      addProcess(input_q_pro_size[i], input_queue_process_id[i], 1);
+    }
+  }
 
   //Drawing Partition
 
@@ -193,14 +331,15 @@ var total_mem_size = 0;
     ctx.fillStyle = "white";
     ctx.fill();
     var i;
-    for (i = 0; i < noOfPartitions; i++) {
+    for (i = 0; i < numberOfPartition; i++) {
       ctx.beginPath();
       ctx.rect(xstart,ystart + partition_start[i] * (500 / total_mem_size), canvasWidth, partition_size[i] * (500 / total_mem_size));
+      ctx.stroke()
       ctx.fillStyle = "#79E0EE";
       ctx.fill();
 
       ctx.font = "14px Arial bold";
-      ctx.fillStyle = "#F2BED1";
+      ctx.fillStyle = "#DB005B";
       ctx.fillText("P-" + String(partition_process_id[i]) + ", size: " + String(partition_size[i]), 50, ystart + partition_start[i] * (500 / total_mem_size) + partition_size[i] * (500 / total_mem_size) / 2);
     }
   }
